4f367d491ded45b7464ec418804c1762c92830d4
Franco Tarulli <franco.tarulli@outlook.es>
2024-05-09 22:22:15 -0300

    Directorios de la pagina web

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Usuarios } from '../data/RegistroUsuario';
+import '../css/HomePage.css';
+
+export const HomePage = (props) => {
+	const { id } = props;
+
+	return <div>HomePage</div>;
+};
---
+++ src/components/RegPage.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const RegPage = () => {
+	return <div>RegPage</div>;
+};
