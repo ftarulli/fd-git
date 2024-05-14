e3605b09bdf8b080803bd8341ef7de0a8a53c191
Pablo Cantos <pablocantos051099@gmail.com>
2024-05-14 17:22:59 -0300

    Comence a armar el sidebar y agregar un poco de estilos

--- src/components/AdminPage.jsx
+++ src/components/AdminPage.jsx
@@ -1,5 +1,26 @@
 import React from 'react';
+import '../css/Adminpage.css';
 
 export const AdminPage = () => {
-	return <div>AdminPage</div>;
+	return (
+		<>
+			<div className="Sidebar">
+				<div className="SidebarUser">
+					<img src="" alt="" />
+					<p>Admin</p>
+				</div>
+				<div className="sideBarControlls">
+					<ul>
+						<li className="OptionSidebar">Reservas</li>
+						<li className="OptionSidebar">Usuarios</li>
+					</ul>
+					<ul>
+						<li className="OptionUsers">Bloqueados</li>
+						<li className="OptionUsers">Nuevos</li>
+						<li className="OptionUsers">Todos</li>
+					</ul>
+				</div>
+			</div>
+		</>
+	);
 };
