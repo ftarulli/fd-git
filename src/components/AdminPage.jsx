ded169a70fa900d0358aaa0b40d221f88e0d8aa4
Pablo Cantos <pablocantos051099@gmail.com>
2024-05-14 18:19:58 -0300

    Instale fontawesome y modifique estilos al sidebar

--- src/components/AdminPage.jsx
+++ src/components/AdminPage.jsx
@@ -6,19 +6,25 @@ export const AdminPage = () => {
 		<>
 			<div className="Sidebar">
 				<div className="SidebarUser">
-					<img src="" alt="" />
+					<img
+						src="https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg"
+						alt="Admin"
+					/>
 					<p>Admin</p>
 				</div>
 				<div className="sideBarControlls">
-					<ul>
-						<li className="OptionSidebar">Reservas</li>
-						<li className="OptionSidebar">Usuarios</li>
-					</ul>
-					<ul>
-						<li className="OptionUsers">Bloqueados</li>
-						<li className="OptionUsers">Nuevos</li>
-						<li className="OptionUsers">Todos</li>
-					</ul>
+					<div>
+						<p className="OptionSidebar">Reservas</p>
+						<p className="OptionSidebar">
+							Usuarios
+							<i className="fa-solid fa-angle-up"></i>
+						</p>
+					</div>
+					<div>
+						<p className="OptionUsers">Bloqueados</p>
+						<p className="OptionUsers">Nuevos</p>
+						<p className="OptionUsers">Todos</p>
+					</div>
 				</div>
 			</div>
 		</>
