d15a07b179f55883b81d82b160ef3eead792e4da
Lourdes Billone <lou.billone@gmail.com>
2024-05-14 11:49:36 -0300

    creo carpeta para imágenes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,32 @@
 import React from "react";
-import "../css/homePage.css";
+import "../css/home.css";
 
 const HomePage = () => {
   return (
     <div>
-      <div className="container">
+      <div className="container-fluid">
         <div className="row">
+          <div className="col d-flex">
+            <hr />
+            <hr />
+          </div>
           <div className="col">
-            <h1>Home</h1>
-            <h2>con css</h2>
+            <div>
+              <h3>Es rápido y divertido!</h3>
+
+              <h2>Rápido y delicioso la calidad en cada plato, sin esperas</h2>
+
+              <p>
+                ¡Reserva tu mesa hoy mismo y prepárate para una experiencia
+                gourmet inolvidable! Desde platos exquisitos hasta un servicio
+                excepcional, te garantizamos una velada que estimulará todos tus
+                sentidos. ¡Haz tu reserva ahora y déjanos sorprenderte con lo
+                mejor de nuestra cocina!
+              </p>
+            </div>
+          </div>
+          <div className="col">
+            <div></div>
           </div>
         </div>
       </div>
