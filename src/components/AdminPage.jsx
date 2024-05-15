fae17e3ace3e46efe35c6580390fe1389053f837
Pablo Cantos <pablocantos051099@gmail.com>
2024-05-15 14:45:29 -0300

    Agregue la seccion con el contador de reservas y usuarios, junto a los estilos necesarios

--- src/components/AdminPage.jsx
+++ src/components/AdminPage.jsx
@@ -9,26 +9,42 @@ export const AdminPage = () => {
 	};
 	return (
 		<>
-			<div className="Sidebar">
-				<div className="SidebarUser">
-					<img
-						src="https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg"
-						alt="Admin"
-					/>
-					<p>Administrador</p>
-				</div>
-				<div className="sideBarControlls">
-					<div>
-						<p className="OptionSidebar">Reservas</p>
-						<p className="OptionSidebar" onClick={ShowOptions}>
-							Usuarios
-							<i className="fa-solid fa-angle-up"></i>
-						</p>
+			<div className='AdminPage'>
+				<div className="Sidebar">
+					<div className="SidebarUser">
+						<img
+							src="https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg"
+							alt="Admin"
+						/>
+						<p>Administrador</p>
 					</div>
-					<div className={clickeado ? 'Options clickeado' : 'Options'}>
-						<p className="OptionUsers">Bloqueados</p>
-						<p className="OptionUsers">Nuevos</p>
-						<p className="OptionUsers">Todos</p>
+					<div className="sideBarControlls">
+						<div>
+							<p className="OptionSidebar">Reservas</p>
+							<p className="OptionSidebar" onClick={ShowOptions}>
+								Usuarios
+								<i className="fa-solid fa-angle-up"></i>
+							</p>
+						</div>
+						<div className={clickeado ? 'Options clickeado' : 'Options'}>
+							<p className="OptionUsers">Bloqueados</p>
+							<p className="OptionUsers">Nuevos</p>
+							<p className="OptionUsers">Todos</p>
+						</div>
+					</div>
+				</div>
+				<div className='ControlNumbers'>
+					<div className='bloquesdatos'>
+						<h2>15</h2>
+						<h3>Reservas</h3>
+					</div>
+					<div className='bloquesdatos'>
+						<h2>15</h2>
+						<h3>Usuarios</h3>
+					</div>
+					<div className='bloquesdatos'>
+						<h2>15</h2>
+						<h3>Usuarios Bloqueados</h3>
 					</div>
 				</div>
 			</div>
