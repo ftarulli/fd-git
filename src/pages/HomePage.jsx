11d475c1d8bf5786b316491a2975e541e4133687
Lourdes Billone <lou.billone@gmail.com>
2024-05-16 22:05:54 -0300

    seccion elegirnos, posiciono elementos como imagen, texto e iconos

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,23 +1,22 @@
 import React from "react";
 import "../css/home.css";
 import principal from "../assets/img_home/principal.png";
-import tacos from "../assets/img_home/tacos.png";
-import hamburguesa from "../assets/img_home/hamburguesa.png";
-import rebosado from "../assets/img_home/rebosado.png";
-import hamburguesaCheddar from "../assets/img_home/hamburguesaCheddar.png";
-import combo from "../assets/img_home/combo.png";
-import pizza from "../assets/img_home/pizza.png";
+import grupo from "../assets/img_home/grupo.png";
+import Hamburger from "../assets/img_home/Iconos/Hamburger.png";
+import Cookie from "../assets/img_home/Iconos/Cookie.png";
+import Wine from "../assets/img_home/Iconos/Wine.png";
 
 const HomePage = () => {
   return (
     <div className="container-general">
+      {/*Sección principal */}
       {/* Container Imagen fondo principal */}
       <div className="container-imagen-principal"></div>
       {/* Container principal */}
       <div className="container-fluid container-principal">
         <div className="row container-columnas">
           {/* Columna íconos principal */}
-          <div className="col col-md-2 container-iconos-principal">
+          <div className="col-10 col-md-2 container-iconos-principal">
             <div className="linea-vertical-principal my-3"></div>
             <div className="iconos-redes-principal">
               <i className="fa fa-facebook" aria-hidden="true"></i>
@@ -53,39 +52,47 @@ const HomePage = () => {
         </div>
       </div>
 
-      <div className="container">
-        <div className="row mx-5">
-          <div className="col-6 ps-3">
-            <div className="fotos my-5">
-              <div className="galeria">
-                <div className="imagen-a">
-                  <img src={tacos} alt="" />
-                </div>
+      {/*Sección elegirnos */}
 
-                <div className="imagen-b">
-                  <img src={hamburguesa} alt="" />
-                </div>
-
-                <div className="imagen-c">
-                  <img src={rebosado} alt="" />
-                </div>
-
-                <div className="imagen-d">
-                  <img src={hamburguesaCheddar} alt="" />
-                </div>
-
-                <div className="imagen-e">
-                  <img src={combo} alt="" />
-                </div>
-
-                <div className="imagen-f">
-                  <img src={pizza} alt="" />
-                </div>
-              </div>
+      <div className="container-fluid container-elegirnos my-5">
+        <div className="row container-filas-elegirnos">
+          <div className="col-10 col-md-5">
+            <div className="imagen-grupo-elegirnos">
+              <img src={grupo} alt="" />
             </div>
           </div>
 
-          <div className="col"></div>
+          <div className="col col-10 col-md-5">
+            <div>
+              <p className="texto_uno">¿Por qué elegirnos?</p>
+              <p className="parrafo_uno-elegirnos">
+                <span className="texto_destacado">Sabor </span> extraordinario y
+                experiencia.
+              </p>
+
+              <p className="parrafo_dos">
+                Sumérgete en una experiencia culinaria sin igual, donde cada
+                bocado es una obra maestra de sabores extraordinarios y cada
+                visita es un viaje inolvidable a través de la excelencia
+                gastronómica. Descubre nuestro compromiso con la calidad y la
+                pasión por la cocina en cada plato que servimos. Bienvenido a un
+                mundo donde el sabor y la experiencia se fusionan para crear
+                momentos inolvidables.
+              </p>
+            </div>
+
+            <div className="container-iconos-elegirnos">
+              <div>
+                <img src={Hamburger} alt="" />
+              </div>
+              <div>
+                <img src={Cookie} alt="" />
+              </div>
+              <div>
+                <img src={Wine} alt="" />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
