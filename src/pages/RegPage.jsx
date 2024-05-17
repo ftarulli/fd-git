389466923cbdc94db4686a13cafbc478fe915df1
Franco Tarulli <franco.tarulli@outlook.es>
2024-05-17 00:52:37 -0300

    Importamos el componente

---
+++ src/pages/RegPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ReservasForm } from '../components/Forms/ReservasForm';
+
+export const RegPage = () => {
+	return (
+		<>
+			<ReservasForm></ReservasForm>
+		</>
+	);
+};
