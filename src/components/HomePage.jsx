aa6d9866e094a152ebb50742dd8a5c20f62ca4b1
Franco Tarulli <franco.tarulli@outlook.es>
2024-05-17 00:56:39 -0300

    Creamos archivo para trabajar en el diseño

--- src/components/HomePage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Usuarios } from '../data/RegistroUsuario';
-import '../css/HomePage.css';
-
-export const HomePage = (props) => {
-	const { id } = props;
-
-	return <div>HomePage</div>;
-};
