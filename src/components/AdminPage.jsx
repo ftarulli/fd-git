d928e45557b8bd81c770dd08605300dfb646f922
Pablo Cantos <pablocantos051099@gmail.com>
2024-05-17 14:30:07 -0300

    Separe el sidebar en un archivo aparte con sus estilos

--- src/components/AdminPage.jsx
+++ src/components/AdminPage.jsx
@@ -2,44 +2,18 @@ import React, { useState } from 'react';
 import '../css/Adminpage.css';
 import { UsersTable } from './UsersTable';
 import { ContadorAdmin } from './ContadorAdmin';
+import { SidebarAdmin } from './SidebarAdmin';
 
 export const AdminPage = () => {
-	const [clickeado, setClickeado] = useState(false);
-
-	const ShowOptions = () => {
-		setClickeado(!clickeado);
-	};
 	return (
 		<>
-			<div className="AdminPage">
-				<div className="Sidebar">
-					<div className="SidebarUser">
-						<img
-							src="https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg"
-							alt="Admin"
-						/>
-						<p>Administrador</p>
-					</div>
-					<div className="sideBarControlls">
-						<div>
-							<p className="OptionSidebar">Reservas</p>
-							<p className="OptionSidebar" onClick={ShowOptions}>
-								Usuarios
-								<i className="fa-solid fa-angle-up"></i>
-							</p>
-						</div>
-						<div className={clickeado ? 'Options clickeado' : 'Options'}>
-							<p className="OptionUsers">Bloqueados</p>
-							<p className="OptionUsers">Nuevos</p>
-							<p className="OptionUsers">Todos</p>
-						</div>
-					</div>
-				</div>
+			<section className="AdminPage">
+				<SidebarAdmin />
 				<div>
 					<ContadorAdmin />
 					<UsersTable />
 				</div>
-			</div>
+			</section>
 		</>
 	);
 };
---
+++ src/components/SidebarAdmin.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import '../css/SidebarAdmin.css';
+
+export const SidebarAdmin = () => {
+	const [clickeado, setClickeado] = useState(false);
+
+	const ShowOptions = () => {
+		setClickeado(!clickeado);
+	};
+
+	return (
+		<div className="Sidebar">
+			<div className="SidebarUser">
+				<img
+					src="https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg"
+					alt="Admin"
+				/>
+				<p>Administrador</p>
+			</div>
+			<div className="sideBarControlls">
+				<div>
+					<p className="OptionSidebar">Reservas</p>
+					<p className="OptionSidebar" onClick={ShowOptions}>
+						Usuarios
+						<i className="fa-solid fa-angle-up"></i>
+					</p>
+				</div>
+				<div className={clickeado ? 'Options clickeado' : 'Options'}>
+					<p className="OptionUsers">Bloqueados</p>
+					<p className="OptionUsers">Nuevos</p>
+					<p className="OptionUsers">Todos</p>
+				</div>
+			</div>
+		</div>
+	);
+};
