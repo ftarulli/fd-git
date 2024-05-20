fd2532595a4911603f111be1e1cf06e4477bc87d
Juan Manuel Gomez Maturana <juanmanuel237@gmail.com>
2024-05-19 21:46:10 -0300

    Vista Galeria imagenes completa

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,20 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { HomePage } from '../components/HomePage';
-import { AdminPage } from '../components/AdminPage';
-import { LoginPage } from '../components/LoginPage';
-import { ErrorPage } from '../components/ErrorPage';
-import { RegPage } from '../components/RegPage';
+import Galeria from '../pages/Galeria';
 
 export const AppRouter = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/admin" element={<AdminPage />} />
-				<Route path="/reg" element={<RegPage />} />
-				<Route path="/" element={<HomePage id={'1'} />} />
-				<Route path="/404" element={<ErrorPage />} />
-				<Route path="/login" element={<LoginPage />} />
+				<Route path="/galeria" element={<Galeria />} />
 			</Routes>
 		</BrowserRouter>
 	);
