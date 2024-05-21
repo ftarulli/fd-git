70a6317304f5a19d3a8be0ff5d86025eaa7bd00c
Juan Manuel Gomez Maturana <juanmanuel237@gmail.com>
2024-05-21 12:38:04 -0300

    saque el import de galeria de imagenes para poder levantar el servidor

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import GaleriaImg from '../pages/GaleriaImg';
 import { Contacto } from '../pages/Contacto';
 
 export const AppRouter = () => {
