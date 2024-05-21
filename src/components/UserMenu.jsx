751dbcbf515c4a315bcaac9585098e7fc1fb2097
Pablo Cantos <pablocantos051099@gmail.com>
2024-05-21 17:19:45 -0300

    Agregue la funcionalidad de eliminar en la tabla de usuarios

--- src/components/UserMenu.jsx
+++ src/components/UserMenu.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
+import apiTest from '../api/api';
 
-export const UserMenu = ({ user, editarUser }) => (
-	<div className="menu">
-		<Button className="menu-item my-1">Eliminar</Button>
-		<Button className="menu-item my-1" onClick={() => editarUser(user)}>
-			Editar
-		</Button>
-	</div>
-);
+export const UserMenu = ({ user, editarUser }) => {
+	const deleteUser = async (_id) => {
+		try {
+			const resp = await apiTest.delete(`/admin/deleteUsers/${_id}`);
+			console.log(resp);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	return (
+		<div className="menu">
+			<Button className="menu-item my-1" onClick={() => deleteUser(user._id)}>
+				Eliminar
+			</Button>
+			<Button className="menu-item my-1" onClick={() => editarUser(user)}>
+				Editar
+			</Button>
+		</div>
+	);
+};
