2710639746b2abd784b4736f5679400261f72e81
Lourdes Billone <lou.billone@gmail.com>
2024-05-22 18:16:54 -0300

    ordeno rutas y modifico nombre de las mismas con minúscula

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,32 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
-import { Contacto } from '../pages/Contacto';
-import HomePage from '../pages/HomePage';
-import { Galeria } from '../pages/Galeria';
-import { Login } from '../pages/Login';
-import { Registro } from '../pages/Registro';
-import { SobreNosotros } from '../pages/SobreNosotros';
-
+import { Contacto } from "../pages/Contacto";
+import HomePage from "../pages/HomePage";
+import { Galeria } from "../pages/Galeria";
+import { Login } from "../pages/Login";
+import { Registro } from "../pages/Registro";
+import { SobreNosotros } from "../pages/SobreNosotros";
 
 export const AppRouter = () => {
-	return (
-		<BrowserRouter>
-			<Routes>
-
-				<Route path="/" element={<HomePage />} />
-				<Route path="/contacto" element={<Contacto />} />
-				<Route path="/Galeria" element={<Galeria />} />
-				<Route path="/Login" element={<Login />} />
-				<Route path="/Registro" element={<Registro />} />
-				<Route path="/sobrenosotros" element={<SobreNosotros />} />
-
-			</Routes>
-			
-		</BrowserRouter>
-	);
-
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/galeria" element={<Galeria />} />
+        <Route path="/sobrenosotros" element={<SobreNosotros />} />
+        <Route path="/contacto" element={<Contacto />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/registro" element={<Registro />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default AppRouter;
