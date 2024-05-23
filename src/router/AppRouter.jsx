6b110c0577ab17080f235e3487aa0d6618d9ffb3
Franco Tarulli <franco.tarulli@outlook.es>
2024-05-22 22:37:24 -0300

    Archivos 404

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,20 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { HomePage } from '../components/HomePage';
-import { AdminPage } from '../components/AdminPage';
-import { LoginPage } from '../components/LoginPage';
-import { ErrorPage } from '../components/ErrorPage';
-import { RegPage } from '../components/RegPage';
+import { ErrorPage } from '../pages/ErrorPage';
 
 export const AppRouter = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/admin" element={<AdminPage />} />
-				<Route path="/reg" element={<RegPage />} />
-				<Route path="/" element={<HomePage id={'1'} />} />
 				<Route path="/404" element={<ErrorPage />} />
-				<Route path="/login" element={<LoginPage />} />
 			</Routes>
 		</BrowserRouter>
 	);
