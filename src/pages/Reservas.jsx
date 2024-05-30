ffa6d7a2ed31a41b2fc02cd85ff71f746a78e1a4
Vanina <vaniibalbastro@gmail.com>
2024-05-29 22:29:30 -0300

    Reservas front hermoso y se ve responsive. chequear con back que este conectado

--- src/pages/Reservas.jsx
+++ src/pages/Reservas.jsx
@@ -1,135 +1,215 @@
 import { useState } from 'react';
 import Swal from 'sweetalert2';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import '../css/reservas.css';
+import {
+  MDBBtn,
+  MDBCol,
+  MDBInput,
+  MDBRow,
+//   MDBTypography
+} from 'mdb-react-ui-kit';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 import apiTest from '../api/apiTest';
 
+import '../css/reservas.css'; // Asegúrate de tener estilos personalizados si es necesario
+import PhoneInput from 'react-phone-input-2'; // Librería para entrada de teléfono
+import 'react-phone-input-2/lib/style.css'; // Estilos de la librería de entrada de teléfono
+
 export const Reservas = () => {
-	const [name, setName] = useState('');
-	const [date, setDate] = useState('');
-	const [time, setTime] = useState('');
-	const [cant, setCant] = useState('');
-	const [phone, setPhone] = useState('');
-	const [email, setEmail] = useState('');
-	const [comment, setComment] = useState('');
+  const [name, setName] = useState('');
+  const [date, setDate] = useState(null);
+  const [time, setTime] = useState(null);
+  const [cant, setCant] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+  const [comment, setComment] = useState('');
 
-	const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const isValidEmail = emailRegex.test(email);
 
-	const isValidEmail = emailRegex.test(email);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
+    if (name === '' || date === null || time === null || cant === '' || phone === '' || email === '' || !isValidEmail) {
+      alert('Por favor completa todos los campos correctamente');
+      return;
+    }
 
-		if (name === '' || date === '' || cant === '') {
-			return console.log('Campos Obligatorios');
-		} else if (!isValidEmail) {
-			console.log('No es un email valido');
-		}
+    try {
+      const resp = await apiTest.post('/auth/saveReservas', {
+        name,
+        date,
+        time,
+        cant,
+        phone,
+        email,
+        comment,
+      });
 
-		registrarReserva(name, date,time, cant, phone, email, comment);
-	};
+      if (resp.status === 201) {
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: 'Registro de mesa exitoso',
+          showConfirmButton: false,
+          timer: 1500,
+        });
 
-	
+        // Limpiar los campos después de enviar la información al backend solo si la solicitud fue exitosa
+        setName('');
+        setDate(null);
+        setTime(null);
+        setCant('');
+        setPhone('');
+        setEmail('');
+        setComment('');
+      }
 
-	const registrarReserva = async (name, date,time, cant, phone, email, comment) => {
-		try {
-			const resp = await apiTest.post('/auth/saveReservas', {
-				name,
-				date,
-				cant,
-				time,
-				phone,
-				email,
-				comment,
-			});
+      console.log(resp);
+    } catch (error) {
+      console.log(error);
+      alert('Hubo un error al procesar tu solicitud. Por favor, inténtalo de nuevo.');
+    }
+  };
 
-			if (resp.status === 201) {
-				Swal.fire({
-					position: 'top-end',
-					icon: 'success',
-					title: 'Registro de mesa exitoso',
-					showConfirmButton: false,
-					timer: 1500,
-				});
-			}
+  return (
+<div className="background-image" style={{ backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.10)), url("src/assets/img-reservas/plato.jpeg")',marginTop:'-50px', backgroundSize: 'cover', backgroundPosition: 'center', minHeight: '100vh' }}>
 
-			console.log(resp);
-		} catch (error) {console.log (error)}
-	};
-	return (
-		<>
-			<div className="img-reservas">
-				<div className="div-str">
-					<strong className="str">Reserva </strong>
-					<strong>tu mesa</strong>
-				</div>
-				<Form onSubmit={handleSubmit} className="from-fst" ccontrolid="name">
-					<Form.Group className="mb-3">
-						<Form.Label className="fromLabel">Nombre y Apellido *</Form.Label>
-						<Form.Control
-							type="text"
-							placeholder="Ej: Silvina Perez"
-							onChange={(e) => setName(e.target.value)}
-						/>
-					</Form.Group>
+    <div className="mx-auto gradient-custom mt-5" style={{ maxWidth: '800px', padding: '20px', borderRadius: '8px' }}>
+      <MDBRow className="pt-3 mx-3">
+        <MDBCol md="12">
+		<div className="text-center mb-4">
+		<h3 className="text-warning">Reservaciones</h3>
+  
+		<h1 className="mb-3" style={{ color: '#FF9800' }}>Reserva tu <span style={{ color: '#FFF' }}>mesa</span></h1>
 
-					<Form.Group className="mt-2" controlId="email">
-						<Form.Label className="fromLabel">Correo electronico *</Form.Label>
-						<Form.Control
-							type="email"
-							placeholder="nombre@gmail.com"
-							onChange={(e) => setEmail(e.target.value)}
-						/>
-					</Form.Group>
+  
+</div>
 
-					<Form.Group className="mt-2" controlId="phone">
-						<Form.Label className="fromLabel">Numero de telefono</Form.Label>
-						<Form.Control
-							type="tel"
-							placeholder="+54 __ __ ___ ___ ___"
-							onChange={(e) => setPhone(e.target.value)}
-						/>
-					</Form.Group>
 
-					<Form.Group className="mt-2" controlid="time">
-						<Form.Label className="fromLabel">Fecha de la reserva *</Form.Label>
-						<DatePicker selected={date} onChange={(e) => setDate(e.target.value)} />
-					</Form.Group>
-
-					<Form.Group className="mt-2" controlid="time">
-						<Form.Label className="fromLabel">Hora de la reserva *</Form.Label>
-						<DatePicker selected={time} onChange={(e) => setTime(e.target.value)} />
-					</Form.Group>
-
-					
-
-					<Form.Group className="mt-2" controlid="cant">
-						<Form.Label className="fromLabel">Numero de comensales *</Form.Label>
-						<Form.Control
-							type="number"
-							placeholder="Ej: 10"
-							onChange={(e) => setCant(e.target.value)}
-						/>
-					</Form.Group>
-
-					<Form.Group className="mt-2" controlId="comment">
-						<Form.Label className="fromLabel">Informacion Adicional</Form.Label>
-						<Form.Control
-							className="fromGroup"
-							type="text"
-							placeholder="Escriba Aqui."
-							onChange={(e) => setComment(e.target.value)}
-						/>
-					</Form.Group>
-
-					<Button className="mt-5 w-100 p-2" variant="primary" type="submit">
-						Registrar Reserva
-					</Button>
-				</Form>
-			</div>
-		</>
-	);
+          <form className="mb-0" onSubmit={handleSubmit}>
+            <MDBRow className="mb-4">
+              <MDBCol md="6">
+                <label className="form-label text-white">Nombre y Apellido *</label>
+                <MDBInput
+                  type='text'
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  className="text-dark" // Cambiado a texto gris oscuro
+                  style={{ backgroundColor: '#FFF', borderColor: '#555' }}
+                  placeholder="Ejemplo: Juan Pablo Martinez"
+                />
+              </MDBCol>
+              <MDBCol md="6">
+                <label className="form-label text-white">Número de teléfono *</label>
+                <PhoneInput
+                  country={'ar'} // Cambia este valor según el país predeterminado
+                  value={phone}
+                  onChange={setPhone}
+                  inputStyle={{ width: '100%', height: '100%', padding: '0.375rem 0.75rem', fontSize: '1rem', lineHeight: '1.5', color: '#000', backgroundColor: '#FFF', borderColor: '#555' }}
+                  containerStyle={{ width: '100%', backgroundColor: '#FFF', color: '#000', borderColor: '#555',borderRadius:'5px'}}
+                />
+              </MDBCol>
+            </MDBRow>
+            <MDBRow className="mb-4">
+              <MDBCol md="6">
+                <label className="form-label text-white">Correo electrónico *</label>
+                <MDBInput
+                  type='email'
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="text-dark" // Cambiado a texto gris oscuro
+                  style={{ backgroundColor: '#FFF', borderColor: '#555' }}
+                  placeholder="Ejemplo: usuario@usuario.com"
+                />
+              </MDBCol>
+              <MDBCol md="6">
+                <label className="form-label text-white">Fecha de la reserva *</label>
+                <DatePicker
+                  selected={date}
+                  onChange={(date) => setDate(date)}
+                  className="form-control text-white date-picker"
+                  dateFormat="dd/MM/yyyy"
+                  autoComplete="off"
+                />
+              </MDBCol>
+            </MDBRow>
+            <MDBRow className="mb-4">
+              <MDBCol md="6">
+                <label className="form-label text-white">Hora de la reserva *</label>
+                <DatePicker
+                  selected={time}
+                  onChange={(time) => setTime(time)}
+                  showTimeSelect
+                  showTimeSelectOnly
+                  timeIntervals={30}
+                  timeCaption="Hora"
+                  dateFormat="h:mm aa"
+                  className="form-control text-white date-picker"
+                  style={{ backgroundColor: '#FFF', borderColor: '#555' }}
+                />
+              </MDBCol>
+              <MDBCol md="6">
+                <label className="form-label text-white">Número de comensales *</label>
+                <div className="select-container">
+                  <select
+                    className="form-select text-white"
+                    value={cant}
+                    onChange={(e) => setCant(e.target.value)}
+                    style={{
+                      backgroundColor: '#FFF',
+                      borderColor: '#555',
+                      color: '#ccc', // Nuevo color del texto
+                    }}
+                  >
+                    <option value="" style={{ color: '#666' }}>Seleccionar cantidad</option>
+                    <option value="1" style={{ color: '#666' }}>1 persona</option>
+                    <option value="2" style={{ color: '#666' }}>2 personas</option>
+                    <option value="3" style={{ color: '#666' }}>3 personas</option>
+                    <option value="4" style={{ color: '#666' }}>4 personas</option>
+                    <option value="5" style={{ color: '#666' }}>5 personas</option>
+                    <option value="6" style={{ color: '#666' }}>6 personas</option>
+                    <option value="7" style={{ color: '#666' }}>7 personas</option>
+                    <option value="8" style={{ color: '#666' }}>8 personas</option>
+                  </select>
+                </div>
+              </MDBCol>
+            </MDBRow>
+            <MDBRow className="mb-4">
+              <MDBCol md="12">
+                <label className="form-label text-white">Información Adicional</label>
+                <textarea
+                  value={comment}
+                  onChange={(e) => setComment(e.target.value)}
+                  className="textarea-control"
+                  placeholder="Escribe aquí..."
+                  style={{ width: '700px', height: '200px', padding: '20px', borderRadius: '5px' }}
+                />
+              </MDBCol>
+            </MDBRow>
+            <div className="text-center">
+              <MDBBtn
+                rounded
+                style={{
+					backgroundColor: 'rgba(0, 0, 0, 0.6)', // Fondo negro con opacidad del 60%
+                  borderColor: '#fff', // Borde blanco
+                  borderWidth: '1px', // Grosor del borde
+                  borderRadius: '5px', // Border radius de 1
+                  color: '#fff', // Color de la fuente blanco
+				width: '120px', // Ancho fijo del botón
+				height: '40px', // Alto fijo del botón
+                }}
+                type="submit"
+                onMouseOver={(e) => e.target.style.backgroundColor = '#FF9800'} // Cambio de color en hover
+                onMouseOut={(e) => e.target.style.backgroundColor = '#000'} // Volver al color original al salir del hover
+              >
+                Confirmar
+              </MDBBtn>
+            </div>
+          </form>
+        </MDBCol>
+      </MDBRow>
+    </div>
+	</div>
+  );
 };
