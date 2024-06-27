1712d3c91ffca6cb7911cf656a5f6a4100fcb33f
Vanina <vaniibalbastro@gmail.com>
2024-06-26 22:56:19 -0300

    probando actualizacion con imagenes netlify

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import App from './App'; // Asegúrate de que esta ruta es correcta
-import './index.css'; // Opcional, si tienes un archivo CSS global
-
-const container = document.getElementById('root');
-const root = createRoot(container);
-
-root.render(<App />);
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,34 +1,37 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Contacto } from '../pages/Contacto';
-import HomePage from '../pages/HomePage';
-import { Galeria } from '../pages/Galeria';
-import { Login } from '../pages/Login';
-import { Registro } from '../pages/Registro';
-import { SobreNosotros } from '../pages/SobreNosotros';
-import ErrorPage from '../pages/ErrorPage';
-import { Reservas } from '../pages/Reservas';
-import { AdminPage } from '../pages/AdminPage';
-import { Footer } from '../components/Footer';
-import { Navegador } from '../components/Navegador';
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Contacto } from "../pages/Contacto";
+import HomePage from "../pages/HomePage";
+import { Galeria } from "../pages/Galeria";
+import { Login } from "../pages/Login";
+import { Registro } from "../pages/Registro";
+import { SobreNosotros } from "../pages/SobreNosotros";
+import ErrorPage from "../pages/ErrorPage";
+import { Reservas } from "../pages/Reservas";
+import { AdminPage } from "../pages/AdminPage";
+import { Footer } from "../components/Footer";
+import { Navegador } from "../components/Navegador";
+
 
 export const AppRouter = () => {
-	return (
-		<BrowserRouter>
-			<Navegador />
-			<Routes>
-				<Route path="/" element={<HomePage />} />
-				<Route path="/galeria" element={<Galeria />} />
-				<Route path="/reservas" element={<Reservas />} />
-				<Route path="/admin" element={<AdminPage />} />
-				<Route path="/sobrenosotros" element={<SobreNosotros />} />
-				<Route path="/contacto" element={<Contacto />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/registro" element={<Registro />} />
-				<Route path="/404" element={<ErrorPage />} />
-			</Routes>
-			<Footer />
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+    <Navegador />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/galeria" element={<Galeria />} />
+        <Route path="/reservas" element={<Reservas />} />
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/sobrenosotros" element={<SobreNosotros />} />
+        <Route path="/contacto" element={<Contacto />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/registro" element={<Registro />} />
+        <Route path="/404" element={<ErrorPage />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+
 };
 
 export default AppRouter;
